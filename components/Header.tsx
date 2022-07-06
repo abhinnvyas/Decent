1c8caec5609e92aed82f69db0d0fd174d92ab6b7
Abhinn Vyas <abhinnvyas2@gmail.com>
2022-07-05 21:42:56 -0700

    Header Component Done.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div className='flex items-center justify-between w-screen p-2 h-14 bg-cyan-300 shadow-md'>
+    <div className='flex items-center justify-between w-screen p-2 h-15 border-b-2
+    bg-stone-700 shadow-lg'>
         <div>
+          <a href="">
             <h1 className="font-serif font-medium  text-xl md:text-xl 
-            lg:text-2xl text-black">Decent</h1>
+            lg:text-2xl text-white">Decent</h1>
+          </a>
         </div>
         <div className="flex row space-x-2 p-2">
-            <button className="bg-cyan-600 p-2 shadow-md text-white rounded-md 
-            hover:shadow-xl transition-all active:scale-95">Login</button>
-            <button className="bg-cyan-600 p-2 shadow-md text-white rounded-md 
-            hover:shadow-xl transition-all active:scale-95">Register</button>
+            <button className="bg-stone-500 p-2 shadow-sm text-white rounded-md 
+            shadow-black hover:shadow-black hover:shadow-md hover:scale-105 transition-all 
+            active:scale-95">Login</button>
+            <button className="bg-stone-500 p-2 shadow-sm text-white rounded-md 
+            shadow-black hover:shadow-black hover:shadow-md hover:scale-105 transition-all 
+            active:scale-95">Register</button>
         </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <div className="">
       <Head>y
         <title>Decent</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/bg-logo.png" />
       </Head>
 
      <Header/>
